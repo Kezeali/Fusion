5c8124af28a37c6c10808fd6bba96a6658b3c2ec
Elliot Hayward <kezeali@gmail.com>
2006-09-20 05:18:04 +0000

    Some RakNet implimentation (starting up the client interface)

--- FusionEngine/FusionNetworkClient.cpp
+++ FusionEngine/FusionNetworkClient.cpp
@@ -5,6 +5,17 @@ using namespace FusionEngine;
 
 FusionNetworkClient::FusionNetworkClient(const std::string &host, const std::string &port)
 {
+	m_RakClient = RakNetworkFactory::GetRakClientInterface();
+	m_RakClient->Connect(host.c_str(),atoi(port.c_str()),atoi(port.c_str()),0,0);
+	m_Host = host;
+	m_Port = port;
+}
+
+FusionNetworkClient::FusionNetworkClient(const std::string &host, const std::string &port,
+																				 ClientOptions *options)
+{
+	m_RakClient = RakNetworkFactory::GetRakClientInterface();
+	m_RakClient->Connect(host.c_str,atoi,<#unsigned short clientPort#>,<#unsigned int depreciated#>,<#int threadSleepTimer#>)
 }
 
 FusionNetworkClient::InitialiseChannel()
