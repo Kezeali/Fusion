522a4235a9eec3b882dd92fd9d43d1a77668af44
Elliot Hayward <kezeali@gmail.com>
2006-09-28 09:01:14 +0000

    Added a pre-compiled version of LinearParticle (a particle engine designed for ClanLib.) Although particles are not yet implemented.

---
+++ FusionEngine/FusionProjectileState.h
@@ -0,0 +1,66 @@
+/*
+  Copyright (c) 2006 Elliot Hayward
+
+  This software is provided 'as-is', without any express or implied warranty.
+	In noevent will the authors be held liable for any damages arising from the
+	use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+		claim that you wrote the original software. If you use this software in a
+		product, an acknowledgment in the product documentation would be
+		appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not
+		be misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef Header_FusionEngine_FusionProjectileState
+#define Header_FusionEngine_FusionProjectileState
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#include "FusionEngineCommon.h"
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+namespace FusionEngine
+{
+	/*!
+	 * \brief
+	 * Holds all state information for each projectile.
+	 *
+	 * An instance of this object holds the state data for each projectile in the game.
+	 * This object should remain general enough to be used by client and server.
+	 *
+	 * \remarks
+	 * When a weapon fires, and instance of this class is produced.
+	 */
+	struct ProjectileState
+	{
+		friend class boost::serialization::access;
+
+		//@{
+		//! Position and velocity vars.
+		CL_Vector Velocity;
+		CL_Vector Position;
+		float Rotation;
+		float RotationalVelocity;
+		//@}
+
+	private:
+		template <class Archive>
+		void serialize(Archive &ar, unsigned int ver);
+	};
+
+}
+
+#endif
---
+++ LinearParticle/README.txt
@@ -0,0 +1,42 @@
+============================================================================
+LinearParticle 0.45
+============================================================================
+LinearParticle is an easy-to-use 2D particle engine for Clanlib. It is a
+simple and small particle engine but powerful enough to be used to make eye
+candy 2D visual effects for games. No graphical editor for LinearParticle
+is available yet. 
+
+LinearParticle 0.45 has been tested with Clanlib 0.8.0. If you are using
+Clanlib 0.8.0 RC2 or earlier, it's recommended upgrading to later version.
+
+
+============================================================================
+LICENSE
+============================================================================
+LinearParticle is distributed under zlib/libpng license. Check the
+"LICENSE" file for details.
+
+
+============================================================================
+News
+============================================================================
+LinearParticle 0.45 ( 27th September 2006 )
+- Moderate changes of API.
+- Added motion controller for handling complex motion of particle(s).
+- Small performance improved ( use 5-10% less CPU time )
+
+
+LinearParticle 0.4 ( 9th July 2006 )
+- First release version
+
+
+
+
+============================================================================
+email : chinfoo@galaxist.net
+website : www.galaxist.net
+- Wong Chin Foo
+
+
+Last Update : 27th September 2006
+============================================================================
