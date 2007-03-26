af82c4939fbbc0a5d344ddf45719daed8a239919
Elliot Hayward <kezeali@gmail.com>
2007-03-26 20:01:49 +0000

    added

---
+++ FusionEngine/FusionMainMenu.h
@@ -0,0 +1,77 @@
+/*
+  Copyright (c) 2006 Fusion Project Team
+
+  This software is provided 'as-is', without any express or implied warranty.
+	In noevent will the authors be held liable for any damages arising from the
+	use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+		claim that you wrote the original software. If you use this software in a
+		product, an acknowledgment in the product documentation would be
+		appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not
+		be misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef Header_FusionEngine_MainMenu
+#define Header_FusionEngine_MainMenu
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#include "FusionCommon.h"
+
+/// Fusion
+#include "FusionGUI.h"
+
+#include "FusionClientOptions.h"
+#include "FusionServerOptions.h"
+
+namespace FusionEngine
+{
+
+	/*!
+	 * \brief
+	 * GUI for main menu
+	 */
+	class MainMenu
+	{
+	public:
+		//! Basic constructor
+		MainMenu();
+		/*!
+		* \brief
+		* Constructor.
+		*/
+		MainMenu(FusionEngine::ClientOptions *clientopts, FusionEngine::ServerOptions *serveropts);
+
+	public:
+		//! Init gui
+		bool Initialise();
+
+		//! Called when the Create button is clicked
+		bool onCreateClicked(const CEGUI::EventArgs& e);
+		//! Called when the Join button is clicked
+		bool onJoinClicked(const CEGUI::EventArgs& e);
+		//! Called when the Options button is clicked
+		bool onOptsClicked(const CEGUI::EventArgs& e);
+
+	protected:
+		//! Options to send to the game
+		ClientOptions *m_ClientOpts;
+		//! Options to send to the game
+		ServerOptions *m_ServerOpts;
+
+	};
+
+}
+
+#endif
---
+++ FusionEngine/FusionOptionsMenu.cpp
@@ -0,0 +1,46 @@
+/// Class
+#include "FusionGUI_Options.h"
+
+/// Fusion
+
+using namespace Fusion;
+
+
+FusionGUI_Options::FusionGUI_Options()
+{
+	m_CurrentScheme = DefaultScheme;
+	m_CurrentLayout = "OptionsMenu";
+}
+
+FusionGUI_Options::FusionGUI_Options(FusionEngine::ClientOptions *clientopts)
+: m_ClientOpts(clientopts)
+{
+	m_CurrentScheme = DefaultScheme;
+	m_CurrentLayout = "OptionsMenu";
+}
+
+bool FusionGUI_Options::Initialise()
+{
+	using namespace CEGUI;
+
+	SchemeManager::getSingleton().loadScheme(m_CurrentScheme);
+
+	WindowManager& winMgr = WindowManager::getSingleton();
+	winMgr.loadWindowLayout(m_CurrentLayout);
+
+	// 'Save' button
+	static_cast<PushButton *> (
+		winMgr.getWindow("OptionsMenu/Save"))->subscribeEvent(
+		PushButton::EventClicked,
+		Event::Subscriber(&FusionGUI_Options::onSaveClicked, this));
+
+	// Call base function (to init KB/Mouse handling)
+	return FusionGUI::Initialise();
+}
+
+bool FusionGUI_Options::onSaveClicked(const CEGUI::EventArgs& e)
+{
+	m_ClientOpts->Save();
+
+	return true;
+}
