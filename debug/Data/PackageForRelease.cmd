e93fce1fd7c9d788565cff9b3010b62a840c2212
Elliot Hayward <kezeali@gmail.com>
2010-06-05 10:00:34 +0000

    Added PackageForRelease.cmd, which creates 7-zip archives in the release/Data folder containing the files in debug/Data/...

---
+++ debug/Data/PackageForRelease.cmd
@@ -0,0 +1,5 @@
+rem The path below works for 32-bit OSes with 32-bit 7-zip and 64-bit OSes with 64-bit 7-zip, so I think that covers most setups
+mkdir ..\..\release\Data
+del ..\..\release\Data\core.7z ..\release\Data\game.7z
+"C:\Program Files\7-Zip\7z.exe" a ..\..\release\Data\core.7z core -xr!*.svn
+"C:\Program Files\7-Zip\7z.exe" a ..\..\release\Data\game.7z Entities Maps "default-clientoptions.xml" "default-controls.xml" gameconfig.xml inputdefinitions.xml -xr!*.svn
