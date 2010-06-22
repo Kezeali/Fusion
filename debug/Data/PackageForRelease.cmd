62a4cfaeb940a845b7a77f54da77c6f5b89e2c80
Elliot Hayward <kezeali@gmail.com>
2010-06-22 23:24:53 +0000

    Made the comment about 7-zip's path more clear

--- debug/Data/PackageForRelease.cmd
+++ debug/Data/PackageForRelease.cmd
@@ -1,4 +1,4 @@
-rem The path below works for 32-bit OSes with 32-bit 7-zip and 64-bit OSes with 64-bit 7-zip, so I think that covers most setups
+rem The path to 7-zip.exe below works for 32-bit OSes with 32-bit 7-zip and 64-bit OSes with 64-bit 7-zip, so I think that covers most setups
 mkdir ..\..\release\Data
 del ..\..\release\Data\core.7z ..\release\Data\game.7z
 "C:\Program Files\7-Zip\7z.exe" a ..\..\release\Data\core.7z @package_core.txt -m"s=off" -xr!*.svn
