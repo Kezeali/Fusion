1669515a83079cd004f4d86937518a1618775b38
Elliot Hayward <kezeali@gmail.com>
2011-12-17 23:37:28 +0000

    Excluded incorrect files being copied by bin/setup.cmd and release/setup_release_package.cmd

--- bin/setup.cmd
+++ bin/setup.cmd
@@ -1,4 +1,4 @@
-robocopy "..\..\FusionDependencies\RakNet_PC-4.033\Lib" "." *.dll *.pdb
+robocopy "..\..\FusionDependencies\RakNet_PC-4.033\Lib" "." *.dll *.pdb /XF *staticdebug.pdb
 robocopy "..\..\FusionDependencies\libRocket\bin" "." *.dll *.pdb
 robocopy "..\..\FusionDependencies\tbb30_174oss\bin\ia32\vc10" "." tbb.* tbbmalloc.* tbb_debug.* tbbmalloc_debug.*
 robocopy "..\..\FusionDependencies\bin" "." *.dll *.pdb
