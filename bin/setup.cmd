88dc07042ae183a86b818945b2071fc463bc439c
Elliot Hayward <kezeali@gmail.com>
2011-12-17 01:50:39 +0000

    Added bin/setup.cmd

---
+++ bin/setup.cmd
@@ -0,0 +1,4 @@
+robocopy "..\..\FusionDependencies\RakNet_PC-4.033\Lib" "." *.dll *.pdb
+robocopy "..\..\FusionDependencies\libRocket\bin" "." *.dll *.pdb
+robocopy "..\..\FusionDependencies\bin" "." *.dll *.pdb
+robocopy "..\..\RAS\library" "." *.dll *.pdb
