473a014007a621fdc47ebbc8aec07e688da132ad
Elliot Hayward <kezeali@gmail.com>
2011-12-17 02:08:20 +0000

    Added line to copy TBB binaries in bin/setup.cmd

--- bin/setup.cmd
+++ bin/setup.cmd
@@ -1,4 +1,5 @@
 robocopy "..\..\FusionDependencies\RakNet_PC-4.033\Lib" "." *.dll *.pdb
 robocopy "..\..\FusionDependencies\libRocket\bin" "." *.dll *.pdb
+robocopy "..\..\FusionDependencies\tbb30_174oss\bin\ia32\vc10" "." tbb.* tbbmalloc.* tbb_debug.* tbbmalloc_debug.*
 robocopy "..\..\FusionDependencies\bin" "." *.dll *.pdb
 robocopy "..\..\RAS\library" "." *.dll *.pdb
