8f44375637e6dcab21911bd342309e7c0dd774f7
Elliot Hayward <kezeali@gmail.com>
2011-12-18 01:28:30 +0000

    release/setup_release_package.cmd now creates a sub-folder

--- release/setup_release_package.cmd
+++ release/setup_release_package.cmd
@@ -1,3 +1,3 @@
-robocopy ..\release ..\release_package\ /MIR /MT /XD .svn AS_DEBUG /XF setup_release_package.cmd *.ilk *.exp *.lib *.pdb
-robocopy ..\bin ..\release_package\ /E /MT /XD .svn AS_DEBUG /XF setup.cmd *.pdb angelscriptd.dll *Debug.dll *_debug.dll *_d.dll 
+robocopy ..\release ..\release_package\Fusion /MIR /MT /XD .svn AS_DEBUG /XF setup_release_package.cmd *.ilk *.exp *.lib *.pdb
+robocopy ..\bin ..\release_package\Fusion /E /MT /XD .svn AS_DEBUG /XF setup.cmd *.pdb angelscriptd.dll *Debug.dll *_debug.dll *_d.dll 
 exit 0
