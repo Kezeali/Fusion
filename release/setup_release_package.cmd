0dc4e9667e40556a9e3fd15fb9b54a9a5204bf9b
Kezeali <kezeali@gmail.com>
2011-12-27 08:49:56 +1300

    release\setup_release_package.cmd now runs bin\setup_targetdirs.cmd if necessary.

--- release/setup_release_package.cmd
+++ release/setup_release_package.cmd
@@ -1,2 +1,3 @@
+if not exist ..\release\angelscript.dll call ..\bin\setup_targetdirs.cmd
 robocopy ..\release ..\release_package\Fusion /MIR /MT /XD .git AS_DEBUG /XF setup_release_package.cmd *.ilk *.exp *.lib *.pdb setup.cmd
 exit 0
