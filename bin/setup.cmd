e83479eff2dd4d628c2eefb77afe05bdf00f7aae
Kezeali <kezeali@gmail.com>
2012-03-01 21:03:44 +1300

    Fixed bin/setup.cmd (the old dll copying batch)

--- bin/setup.cmd
+++ bin/setup.cmd
@@ -1,5 +1,7 @@
+rem This is the old setup script that copies the lib binaries into bin
+rem You should use setup_targetdirs.cmd instead
 robocopy "..\..\FusionDependencies\RakNet_PC-4.033\Lib" "." *.dll *.pdb /XF *staticdebug.pdb
 robocopy "..\..\FusionDependencies\libRocket\bin" "." *.dll *.pdb
-robocopy "..\..\FusionDependencies\tbb30_174oss\bin\ia32\vc10" "." tbb.* tbbmalloc.* tbb_debug.* tbbmalloc_debug.*
+robocopy "..\..\FusionDependencies\tbb_oss\bin\ia32\vc10" "." tbb.* tbbmalloc.* tbb_debug.* tbbmalloc_debug.*
 robocopy "..\..\FusionDependencies\bin" "." *.dll *.pdb
 robocopy "..\..\RAS\library" "." *.dll *.pdb
