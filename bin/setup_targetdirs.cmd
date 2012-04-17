ad6ee4caf5ae6099e82ecc9fef99e9ac31a308ad
Kezeali <kezeali@gmail.com>
2012-04-17 22:20:21 +1200

    Modified setup_targetdirs.cmd to copy angelscript binaries from the location they are built in.

--- bin/setup_targetdirs.cmd
+++ bin/setup_targetdirs.cmd
@@ -7,8 +7,11 @@ robocopy "..\..\FusionDependencies\libRocket\bin" "..\release" *.dll *.pdb /XF *
 robocopy "..\..\FusionDependencies\tbb_oss\bin\ia32\vc10" "..\debug" tbb_debug.* tbbmalloc_debug.*
 robocopy "..\..\FusionDependencies\tbb_oss\bin\ia32\vc10" "..\release" tbb.* tbbmalloc.*
 
+robocopy "..\..\FusionDependencies\angelscriptDev\sdk\angelscript\lib" "..\debug" *d.dll *d.pdb
+robocopy "..\..\FusionDependencies\angelscriptDev\sdk\angelscript\lib" "..\release" *.dll /XF *d.dll
+
 robocopy "..\..\FusionDependencies\bin" "..\debug" physfs.dll *d.dll *d.pdb
-robocopy "..\..\FusionDependencies\bin" "..\release" *.dll
+robocopy "..\..\FusionDependencies\bin" "..\release" *.dll /XF *d.dll
 
 robocopy "..\..\RAS\library" "..\debug" *_d.dll *_d.pdb
 robocopy "..\..\RAS\library" "..\release" *.dll /XF *_d.dll
