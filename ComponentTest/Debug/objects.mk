10a266f4877e735866201683698f9cc8cac15b78
Elliot Hayward <kezeali@gmail.com>
2012-12-30 12:34:30 +1300

    Updated git ignores and deleted up old linux makefiles.

--- ComponentTest/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lEngineFramework -lSystemClanLibRendering -lSystemBox2D -lSystemAngelScript -lRocketAngelScriptControls -lRocketAngelScript -lComponentInterface -lVisual -lScriptUtils -lNetwork -lInput -lCommon -lBox2D -lkyotocabinet -lRakNetDynamic -langelscript -lphysfs -lminizip -ltinyxml -lyaml-cpp -lboost_date_time -lboost_iostreams -lboost_program_options -lboost_filesystem -lboost_system -lboost_thread -ltbb -ltbbmalloc -lz
-
--- UnitTests/CommonTests/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lCommon -ltinyxml -lminizip -lphysfs -langelscript -lclan23Core -lboost_date_time -ltbb -ltbbmalloc -lgtest
-
