299265a1e486de6ed6ec94d8f1ccaf4e68a96833
Elliot Hayward <kezeali@gmail.com>
2012-12-30 21:26:46 +1300

    Added the script used to generate FusionDependencies.7z

---
+++ tools/MakeWindowsDepsPackage/CompressDeps.cmd
@@ -0,0 +1,3 @@
+cd ..\..\..\FusionDependencies
+if exist FusionDependencies.7z del FusionDependencies.7z
+"%ProgramFiles%\7-Zip\7z.exe" a FusionDependencies.7z @filelist.txt -mx9 -ssc -xr!*.ncb -xr!*.sdf -xr!*.user -xr!*.suo -xr!*/Debug -xr!*/Release -xr!tbb_oss/doc -xr!*/ipch -xr!*.obj -xr!*.manifest -xr!*.manifest.res -xr!kyotocabinet/*.exe
